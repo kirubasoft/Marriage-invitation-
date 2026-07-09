ddc3668da90f3a14a8d6c7b472ecec6ee7204f7b
Kirubakaran <156403015+kirubasoft@users.noreply.github.com>
2026-07-09 23:45:43 +0530

    Create Script.cs

---
+++ Script.cs
@@ -0,0 +1,4 @@
+document.getElementById("openCard").onclick = function () {
+    document.getElementById("openCard").style.display = "none";
+    document.getElementById("card").style.display = "block";
+};
